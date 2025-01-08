53308443af172988815f852dd6653da5d12a6757
ALI254746! <hojiakbarmuhammad77@gmail.com>
2025-01-08 11:35:07 +0500

    html bilan ishlash node.js

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
 const server = http.createServer((req, res) => {
     if (req.method === 'GET') {
-        res.writeHead(200, {"Content-Type": 'text/html'});
-        res.end(`
-            <h4>Email yuboring</h4>
-            <form method='post' action='/'>
-                <input name="email" type='email' placeholder="Emailingizni kiriting" />
-                <button type="submit">Email yuborish</button>
-            </form>
-        `);
+        if (req.url === '/') {
+            fs.readFile(path.join(__dirname, 'modules', 'templates', 'index.html'), 'utf-8', (err, content) => {
+                if (err) throw err;
+                res.end(content);
+            });
+        } else if (req.url === "/about") {
+            fs.readFile(path.join(__dirname, 'modules', 'templates', 'about.html'), 'utf-8', (err, content) => {
+                if (err) throw err;
+                res.end(content);
+            });
+        }else if(req.url ==='/contact'){
+            fs.readFile(path.join(__dirname,'modules','templates','contact.html'),'utf-8',(err,content)=>{
+                if(err) throw err;
+                res.end(content)
+            });
+        }
     } else if (req.method === 'POST') {
-        const email=[];
-        res.writeHead(200,{'content-type': 'text/html;charset=utf-8'})
-        req.on('data',data =>{
-            email.push(Buffer.from(data))
-        })
-        req.on("end", ()=>{
-           const m=email.toString().split('=')[1];
-           console.log(m);
-           res.end(`email added:${m}`);
-        })
-       
+        const body = [];
+        res.writeHead(200, {'content-type': 'text/html;charset=utf-8'});
+        req.on('data', data => {
+            body.push(Buffer.from(data));
+        });
+        req.on('end', () => {
+            const m = body.toString().split('=')[1];
+            console.log(m);
+            res.end(`Email muvaffaqiyatli qo'shildi: ${m}`);
+        });
     }
 });
 
